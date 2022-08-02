20d96b099e62e0b0dd91b8f767e0f9787f087720
Saeful_A <saefulanwaroktariansah@gmail.com>
2022-08-02 20:25:58 +0700

    Update 2 27/07/2022

--- app/src/main/java/com/example/tubes_akb/HomeFragment.java
+++ app/src/main/java/com/example/tubes_akb/HomeFragment.java
@@ -31,10 +31,10 @@ import com.google.android.gms.tasks.Task;
 
 public class HomeFragment extends Fragment {
 
-    private SupportMapFragment mapFragment;
+//    private SupportMapFragment mapFragment;
 //    private FusedLocationProviderClient client;
-    private Button btnFind;
-    private double currentLat = 0, currentLong = 0;
+//    private Button btnFind;
+//    private double currentLat = 0, currentLong = 0;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -47,53 +47,55 @@ public class HomeFragment extends Fragment {
         View fragment = inflater.inflate(R.layout.fragment_home, container, false);
 
 //        client = LocationServices.getFusedLocationProviderClient(getActivity());
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-
+//        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+//
 //        btnFind = (Button) fragment.findViewById(R.id.btn_find);
+//
+//        getCurrentLocation();
 
-        getCurrentLocation();
-
-        btnFind.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mapFragment.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(@NonNull GoogleMap googleMap) {
-                        LatLng lokasi1 = new LatLng(-6.88473319759774, 107.61640276608132);
-                        LatLng lokasi2 = new LatLng(-6.884381698848637, 107.61411752401223);
-                        LatLng lokasi3 = new LatLng(-6.884403001811196, 107.61349525152424);
-                        LatLng lokasi4 = new LatLng(-6.882826579999446, 107.61364545522824);
-                        LatLng lokasi5 = new LatLng(-6.884871666764502, 107.61701430973018);
-
-                        MarkerOptions options1 = new MarkerOptions().position(lokasi1).title("Dapur Eyang Restaurant");
-                        MarkerOptions options2 = new MarkerOptions().position(lokasi2).title("Ikan Laut Sambal Pesisir");
-                        MarkerOptions options3 = new MarkerOptions().position(lokasi3).title("Yagami Ramen House Dago");
-                        MarkerOptions options4 = new MarkerOptions().position(lokasi4).title("Tizi Cake Shop & Restaurant");
-                        MarkerOptions options5 = new MarkerOptions().position(lokasi5).title("Raja Basa 9");
-
-                        googleMap.addMarker(options1);
-                        googleMap.addMarker(options2);
-                        googleMap.addMarker(options3);
-                        googleMap.addMarker(options4);
-                        googleMap.addMarker(options5);
-
-                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lokasi2, 16));
-                    }
-                });
-            }
-        });
+//        btnFind.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mapFragment.getMapAsync(new OnMapReadyCallback() {
+//                    @Override
+//                    public void onMapReady(@NonNull GoogleMap googleMap) {
+//                        LatLng lokasi1 = new LatLng(-6.88473319759774, 107.61640276608132);
+//                        LatLng lokasi2 = new LatLng(-6.884381698848637, 107.61411752401223);
+//                        LatLng lokasi3 = new LatLng(-6.884403001811196, 107.61349525152424);
+//                        LatLng lokasi4 = new LatLng(-6.882826579999446, 107.61364545522824);
+//                        LatLng lokasi5 = new LatLng(-6.884871666764502, 107.61701430973018);
+//
+//                        MarkerOptions options1 = new MarkerOptions().position(lokasi1).title("Dapur Eyang Restaurant");
+//                        MarkerOptions options2 = new MarkerOptions().position(lokasi2).title("Ikan Laut Sambal Pesisir");
+//                        MarkerOptions options3 = new MarkerOptions().position(lokasi3).title("Yagami Ramen House Dago");
+//                        MarkerOptions options4 = new MarkerOptions().position(lokasi4).title("Tizi Cake Shop & Restaurant");
+//                        MarkerOptions options5 = new MarkerOptions().position(lokasi5).title("Raja Basa 9");
+//
+//                        googleMap.addMarker(options1);
+//                        googleMap.addMarker(options2);
+//                        googleMap.addMarker(options3);
+//                        googleMap.addMarker(options4);
+//                        googleMap.addMarker(options5);
+//
+//                        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lokasi2, 16));
+//                    }
+//                });
+//            }
+//        }
+//        );
 
         return fragment;
     }
+}
 
-    private void getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(mapFragment.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                (mapFragment.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(),
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-        }
+//    private void getCurrentLocation() {
+//        if (ActivityCompat.checkSelfPermission(mapFragment.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+//                (mapFragment.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(getActivity(),
+//                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+//        }
 
 //        Task<Location> task = client.getLastLocation();
 //        task.addOnSuccessListener(new OnSuccessListener<Location>() {
@@ -114,5 +116,5 @@ public class HomeFragment extends Fragment {
 //                }
 //            }
 //        });
-    }
-}
+//    }
+//}
